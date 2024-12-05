a9fe3e74ac5d4f36a63a23621c537eb9a559d8da
Samuel Campelo <samue@LENOVOSAMUEL>
2024-12-05 11:53:33 -0300

    Adicionar arquivos de projeto.

---
+++ album.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum \"{Nome}\":");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"- {musica.Nome}");
+        }
+        Console.WriteLine($"Duração total do álbum: {DuracaoTotal} segundos.");
+    }
+}
